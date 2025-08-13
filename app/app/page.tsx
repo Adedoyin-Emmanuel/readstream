bf4bef1c9d2c3adc5fd13d0b6f3e3ccf669ac078
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-08-13 15:46:23 +0100

    chore: update landing page

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,20 +1,21 @@
-import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-
-        <h1 className="text-2xl">I need to catch that</h1>
-      </main>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <section className="flex flex-col gap-4 md:w-auto w-11/12">
+        <h1 className="text-4xl font-bold">Readstream</h1>
+        <p className="text-muted-foreground">
+          A simple tool to upload and preview README files in your browser
+        </p>
+        <Link href="/app">
+          <Button size={"lg"} className="cursor-pointer w-full mt-2">
+            Get started
+          </Button>
+        </Link>
+      </section>
     </div>
   );
 }
