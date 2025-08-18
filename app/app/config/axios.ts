2221d479b87e813e4fb3dfb6d8649c6ee72c69df
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-08-18 10:17:18 +0100

    chore: update axios base config

--- app/app/config/axios.ts
+++ app/app/config/axios.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
 const Axios = axios.create({
-    baseURL: 
-})
+  baseURL: process.env.NEXT_PUBLIC_API_URL as string,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default Axios;
